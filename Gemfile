230007eb5280b98347075af07b3a1badeb611a35
somendraproton <somendra.thakur@protonshub.in>
2021-11-02 16:17:54 +0530

    Added active_admin and bootstrap configured

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 
 gem 'devise'
+gem 'activeadmin'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
---
+++ app/admin/users.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :contact, :address
+  #
+  # or
+  #
+  permit_params do
+    permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :contact, :address]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  index do
+    column :email
+    
+    actions
+  end
+
+  
+end
